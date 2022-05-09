0d0ff5e383dad82656a5014a5ba6ab652ea80381
asi1117 <luy984361@gmail.com>
2022-05-09 19:37:48 +0900

    爬虫练习2

--- pctest1/test3.py
+++ pctest1/test3.py
@@ -4,6 +4,6 @@ s= input("请输入你要翻译的英文单词")
 dat= {
     "kw" :s
 }
-#发送post请求
+#发送post请求，发送的数据必须放在字典中，通过data进行传递
 resp = requests.post(url,data=dat)
-print(resp.text)
+print(resp.json())#将服务器返回的内容直接处理成json（） -dict
